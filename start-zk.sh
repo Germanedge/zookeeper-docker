c4487e3837b8b3070ac039d0a367df14773c68da
Björn Stöbe <bjoern.stoebe@qda-solutions.com>
2023-03-16 10:33:06 +0100

    Pre-GE4-4940: Fix ownership of zookeeper data files, while container still runs as root

--- start-zk.sh
+++ start-zk.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/sh	
 
 sed -i -r 's|#(log4j.appender.ROLLINGFILE.MaxBackupIndex.*)|\1|g' $ZK_HOME/conf/log4j.properties
 sed -i -r 's|#autopurge|autopurge|g' $ZK_HOME/conf/zoo.cfg
@@ -8,5 +8,17 @@ sed -i -r 's|#metricsProvider|metricsProvider|g'  $ZK_HOME/conf/zoo.cfg
 
 export ZOO_LOG4J_PROP="INFO,CONSOLE,ROLLINGFILE"
 
+# since we are root, we can change the ownership of zookeeper files
+
+current_uid=$(stat -c "%u" ${ZK_HOME}"/data")
+current_gid=$(stat -c "%g" ${ZK_HOME}"/data")
+
+if [ $current_uid != 1000 ] || [ $current_gid != 1000 ]; then
+  echo "[Edge.One][*** MIGRATION START ***] Change ownership of zookeeper data files. This can take several minutes."
+  chown -R 1000:1000 $ZK_HOME/data
+  echo "[Edge.One][*** MIGRATION END ***] Change ownership of zookeeper files finished. Zookeeper will now start."
+else
+  echo "[Edge.One][INFO] Zookeeper data files are owned by $current_uid:$current_gid."
+fi
 
 /opt/zookeeper/bin/zkServer.sh start-foreground
